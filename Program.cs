b413800630db40cf0c4365fb64efb2814195dafe
bkea <hawaiianrebel@cox.net>
2021-12-04 23:23:35 -0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace TestQuestion1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var result = Solution.findLargest();
+            Console.WriteLine("For {0} the largest value is {1}", "area",result);
+        }
+    }
+}
---
+++ Solution.cs
@@ -0,0 +1,54 @@
+﻿// you can also use other imports, for example:
+// using System.Collections.Generic;
+
+// you can write to stdout for debugging purposes, e.g.
+// Console.WriteLine("this is a debug message");
+
+public static class Solution
+{
+    private static int _m = 0;
+    private static bool columnFound = false;
+
+    public static int findLargest()
+    {
+        int result = 0;
+        foreach (string line in System.IO.File.ReadLines(@"c:\temp\test.csv"))
+        {
+            result = solution(line, "area");
+
+        }
+        return result;
+    }
+    private static int solution(string S, string C)
+    {
+        int n = 0;
+        int i = 0;
+
+        string[] rowcolumns = S.Split(',');
+        if (columnFound)
+        {
+            if (int.TryParse(rowcolumns[_m], out int j))
+            {
+                n = j > n ? j : n;
+            }
+        }
+        else
+        {
+            foreach (var col in rowcolumns)
+            {
+                if (col == C)
+                {
+                    _m = i;
+                    columnFound = true;
+                    continue;
+                }
+                else
+                {
+                    i++;
+                }
+            }
+        }
+       
+        return n;
+    }
+}
